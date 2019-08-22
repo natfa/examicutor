58fcc5789f9b342f6a63c3bf2c8d5c3d15a71c21
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-08-22 15:44:31 +0200

    Missed two files

---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import mysql from 'mysql'
+
+import config from '../config/default';
+
+export default mysql.createPool({
+  host: config.db.host,
+  user: config.db.user,
+  password: config.db.password,
+  database: config.db.database,
+  waitForConnections: true,
+});
