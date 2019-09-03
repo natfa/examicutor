e924f52c1959ccb037e6a346cecbe4f0591cc5be
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-09-03 15:09:32 +0200

    Add sql scripts to facilitate new subjects with themes

--- sqlscripts/insert_dummy.sql
+++ sqlscripts/insert_dummy.sql
@@ -1,7 +1,28 @@
-INSERT INTO `questions` (text)
-  VALUES ("Which programming language is used for this project?");
+INSERT INTO `subjects` (name) VALUES
+  ("Math");
+SET @sid = LAST_INSERT_ID();
+INSERT INTO `themes` (name, subjectid, themeid) VALUES
+  ("Geometry", @sid, NULL);
+SET @tid = LAST_INSERT_ID();
+INSERT INTO `themes` (name, subjectid, themeid) VALUES
+  ("Trigonometry", @sid, @tid);
+
+
+INSERT INTO `subjects` (name) VALUES
+  ("Programming");
+SET @sid = LAST_INSERT_ID();
+INSERT INTO `themes` (name, subjectid, themeid) VALUES
+  ("Software development", @sid, NULL),
+  ("Web development", @sid, NULL);
+SET @tid = LAST_INSERT_ID();
+INSERT INTO `themes` (name, subjectid, themeid) VALUES
+  ("NodeJS", @sid, @tid);
+
+
+INSERT INTO `questions` (text, points)
+  VALUES ("Which programming language is used for this project?", 2);
 SET @qid = LAST_INSERT_ID();
-INSERT INTO `answers` (text, correct, questionid) values
+INSERT INTO `answers` (text, correct, questionid) VALUES
   ("C#", FALSE, @qid),
   ("Javascript", TRUE, @qid),
   ("Typescript", TRUE, @qid),
@@ -9,20 +30,20 @@ INSERT INTO `answers` (text, correct, questionid) values
   ("Golang", FALSE, @qid);
 
 
-INSERT INTO `questions` (text)
-  VALUES ("Which database is used for this project?");
+INSERT INTO `questions` (text, points)
+  VALUES ("Which database is used for this project?", 3);
 SET @qid = LAST_INSERT_ID();
-INSERT INTO `answers` (text, correct, questionid) values
+INSERT INTO `answers` (text, correct, questionid) VALUES
   ("MongoDB", FALSE, @qid),
   ("MySQL", TRUE, @qid),
   ("PostgreSQL", FALSE, @qid),
   ("MS SQL", FALSE, @qid);
 
 
-INSERT INTO `questions` (text)
-  VALUES ("What does this picture represent?");
+INSERT INTO `questions` (text, points)
+  VALUES ("What does this picture represent?", 1);
 SET @qid = LAST_INSERT_ID();
-INSERT INTO `answers` (text, correct, questionid) values
+INSERT INTO `answers` (text, correct, questionid) VALUES
   ("An old lady", FALSE, @qid),
   ("A cute girl", TRUE, @qid),
   ("A batka", FALSE, @qid),
--- sqlscripts/setup.sql
+++ sqlscripts/setup.sql
@@ -1,6 +1,28 @@
 DROP TABLE IF EXISTS `answers`;
 DROP TABLE IF EXISTS `media`;
 DROP TABLE IF EXISTS `questions`;
+DROP TABLE IF EXISTS `themes`;
+DROP TABLE IF EXISTS `subjects`;
+
+
+CREATE TABLE `subjects` (
+  `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL
+);
+
+
+CREATE TABLE `themes` (
+  `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `subjectid` INT UNSIGNED NOT NULL,
+  `themeid` INT UNSIGNED,
+  FOREIGN KEY (subjectid)
+    REFERENCES subjects (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (themeid)
+    REFERENCES themes (id)
+    ON DELETE CASCADE
+);
 
 
 CREATE TABLE `questions` (
