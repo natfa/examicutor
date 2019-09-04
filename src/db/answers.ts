808073082d550d099cb1fae51649f46e94e1db48
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-09-04 11:14:10 +0200

    Implement database functionality for answers

---
+++ src/db/answers.ts
@@ -0,0 +1,61 @@
+import { query } from './index'
+import Answer from '../models/Answer'
+
+function updateAnswer(answerId: string, text: string, correct: boolean): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    query({
+      sql: `update answers set
+      text = ?
+      correct = ?
+      where answers.id = ?`,
+      values: [text, correct, answerId],
+    })
+      .then((results) => {
+        if (results.affectedRows !== 1)
+          return resolve(false)
+        return resolve(true)
+      })
+      .catch((err) => {
+        return reject(err)
+      })
+  })
+}
+
+function deleteAnswer(answerId: string): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    query({
+      sql: `delete from answers
+      where answers.id = ?`,
+      values: [answerId],
+    })
+      .then((results) => {
+        if (results.affectedRows !== 1)
+          return resolve(false)
+        return resolve(true)
+      })
+      .catch((err) => {
+        return reject(err)
+      })
+  })
+}
+
+function saveAnswer(questionId: string, answer: Answer): Promise<Answer|null> {
+  return new Promise<Answer|null>((resolve, reject) => {
+    query({
+      sql: `insert into answers
+      (text, correct, questionid) values
+      (?, ?, ?)`,
+      values: [answer.text, answer.correct, questionId],
+    })
+      .then((results) => {
+        if (results.affectedRows !== 1)
+          return resolve(null)
+
+        answer.id = results.insertId
+        return resolve(answer)
+      })
+      .catch((err) => {
+        return reject(err)
+      })
+  })
+}
