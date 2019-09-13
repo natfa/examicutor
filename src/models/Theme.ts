fab24415b83f73a5c0e33152d609decc05bd4c1a
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-09-13 14:30:35 +0200

    Create theme model

---
+++ src/models/Theme.ts
@@ -0,0 +1,17 @@
+class Theme {
+  id: string|undefined|null
+  name: string
+  subjectid: string
+
+  constructor(
+    id: string|undefined|null,
+    name: string,
+    subjectid: string,
+  ) {
+    this.id = id
+    this.name = name
+    this.subjectid = subjectid
+  }
+}
+
+export default Theme
