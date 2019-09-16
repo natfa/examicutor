e6c76dc00ef237765ef4249eb87776761ed7bd14
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-09-16 14:23:32 +0200

    Completely revamp questions endpoints

--- src/controllers/question.ts
+++ src/controllers/question.ts
@@ -5,115 +5,120 @@ import fs from 'fs'
 
 import { validateQuestionBody, validateFilters } from '../validators/question'
 import questiondb from '../db/questions'
+import subjectdb from '../db/subjects'
 import themedb from '../db/themes'
 
 import Question from '../models/Question'
-import { IQuestionFilters } from '../models/IQuestionFilters'
 import Answer from '../models/Answer'
+import Theme from '../models/Theme'
+import Subject from '../models/Subject'
+
+const getQuestions = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const questions = await questiondb.getMany(100)
+    return res.status(200).send(questions)
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+const getQuestionById = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const question = await questiondb.getOneById(req.params.id)
+
+    if (question === null)
+      return res.status(404).send('Not Found')
+    return res.status(200).send(question)
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+const getQuestionsByFilter = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { subjectid, text } = req.params
+
+    const questions = await questiondb.getManyBySubjectid(subjectid)
+    return res.status(200).send(questions)
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+const createQuestion = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { text, subject, theme, points, correctAnswers, incorrectAnswers } = req.body
+    const uploadsDir = path.resolve('./uploads')
+
+    let answers = [
+      ...correctAnswers.map((a: string) => new Answer(null, a, true)),
+      ...incorrectAnswers.map((a: string) => new Answer(null, a, false)),
+    ]
+
+    // Apparently typescript's type definitions for multer are
+    // fucked up, so you gotta use the any escape :D
+    const files: any = req.files
+    const fileBuffers: Array<Buffer> = files.map((file: any) => {
+      return fs.readFileSync(path.resolve(file.path))
+    })
+
+    let subjectFound: Subject|null
+    let themeFound: Theme|null
+
+    [subjectFound, themeFound] = await Promise.all([
+      subjectdb.getOneByName(subject),
+      themedb.getOneByName(theme),
+    ])
+
+    if (subjectFound === null || subjectFound.id === null || subjectFound.id === undefined)
+      return res.status(400).send(`Subject doesn't exist`)
+
+    if (themeFound === null)
+      themeFound = await themedb.saveOne(new Theme(null, theme, subjectFound.id))
+
+
+    let question = new Question(
+      null,
+      text,
+      answers,
+      Number(points),
+      subjectFound,
+      themeFound,
+      fileBuffers,
+    )
+
+    question = await questiondb.saveOne(question)
+    return res.status(200).send(question)
+  }
+  catch(err) {
+    next(err)
+  }
+}
+
+const deleteQuestion = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = req.params.id
+
+    const success = await questiondb.deleteOneById(id)
+    if (!success)
+      return res.status(404).send('Not Found')
+    return res.status(204).send('No Content')
+  }
+  catch(err) {
+    next(err)
+  }
+}
 
 const router = express.Router()
 const upload = multer({ dest: 'uploads/'})
 
-// GET /question/
-// Get all questions
-router.get('/', (req, res, next) => {
-  questiondb.getAll()
-    .then((questions) => {
-      return res.status(200).send(questions);
-    })
-    .catch((err) => {
-      return next(err)
-    })
-});
-
-// GET /question/subject/text
-// Get's questions while applying fiters
-router.get('/filter/:subject/:text?', validateFilters, (req, res, next) => {
-  const { subject, text } = req.params
-
-  const filters: IQuestionFilters = { subject, text }
-  questiondb.getAll(filters)
-    .then((questions) => {
-      return res.status(200).send(questions)
-    })
-    .catch((err) => {
-      return res.status(500).send(err)
-    })
-})
-
-// GET /question/questionID
-// Get a question by it's ID
-router.get('/:questionID', (req, res, next) => {
-  questiondb.getById(req.params.questionID)
-    .then((question) => {
-      if (!question)
-        return res.status(404).send('Not Found');
-      return res.status(200).send(question);
-    })
-    .catch((err) => {
-      return next(err)
-    });
-})
-
-// POST /quesiton/
-// Create a new question
-router.post('/', upload.array('media', 10), validateQuestionBody, (req, res, next) => {
-  const { text, subject, theme, points, correctAnswers, incorrectAnswers } = req.body
-  const uploadsDir = path.resolve('./uploads')
-
-  let answers: Array<Answer> = []
-  correctAnswers.map((a: string) => answers = [...answers, new Answer(null, a, true)])
-  incorrectAnswers.map((a: string) => answers = [...answers, new Answer(null, a, false)])
-
-  // Apparently typescript's type definitions for multer are
-  // fucked up, so you gotta use the any escape :D
-  const files: any = req.files
-  const fileBuffers: Array<Buffer> = files.map((file: any) => {
-    return fs.readFileSync(path.resolve(file.path))
-  })
-
-  const newQuestion = new Question(
-    null,
-    text,
-    answers,
-    Number(points),
-    subject,
-    theme || null,
-    fileBuffers,
-  )
-
-  themedb.exists(theme)
-    .then((exists) => {
-      if (!exists) {
-        themedb.save(subject, theme)
-          .then((success) => {
-            if(!success)
-              return res.status(500).send('Internal server error')
-          })
-      }
-      questiondb.save(newQuestion)
-        .then((question) => {
-          if (!question) {
-            return res.status(500).send('Internal Server Error')
-          }
-          return res.status(200).send(question)
-        })
-        .catch((err) => {
-          return next(err)
-        })
-    })
-})
-
-router.delete('/:questionID', (req, res) => {
-  questiondb.deleteById(req.params.questionID)
-    .then((success) => {
-      if (!success)
-        return res.status(404).send('Not Found')
-      return res.status(204).send('No Content')
-    })
-    .catch((err) => {
-      return res.status(500).send(err)
-    })
-})
+router.get('/', getQuestions)
+router.get('/:id', getQuestionById)
+router.get('/filter/:subjectid/:text?', validateFilters, getQuestionsByFilter)
+router.post('/', upload.array('media', 10), validateQuestionBody, createQuestion)
+router.delete('/:id', deleteQuestion)
 
 export default router
