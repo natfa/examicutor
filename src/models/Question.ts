04512c03ff0d3ac891fc38898261f3d7601b0c64
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-09-16 13:17:27 +0200

    Add a more basic question model

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -1,21 +1,23 @@
 import Answer from './Answer'
+import Subject from './Subject'
+import Theme from './Theme'
 
 class Question {
-  id: string|null
+  id: string|null|undefined
   text: string
   answers: Array<Answer>
   points: number
-  subject: string
-  theme: string|null
+  subject: Subject
+  theme: Theme
   media: Array<Buffer>
 
   constructor (
-    id: string|null = null,
+    id: string|null|undefined,
     text: string,
     answers: Array<Answer>,
     points: number,
-    subject: string,
-    theme: string|null,
+    subject: Subject,
+    theme: Theme,
     media: Array<Buffer>,
   ) {
     this.id = id
@@ -25,6 +27,28 @@ class Question {
     this.subject = subject
     this.theme = theme
     this.media = media
+  }
+}
+
+export class QuestionBase {
+  id: string
+  text: string
+  points: number
+  subject: Subject
+  theme: Theme
+
+  constructor(
+    id: string,
+    text: string,
+    points: number,
+    subject: Subject,
+    theme: Theme,
+  ) {
+    this.id = id
+    this.text = text
+    this.points = points
+    this.subject = subject
+    this.theme = theme
   }
 }
 
