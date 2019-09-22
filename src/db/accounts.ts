a3f6101acab39410fc9ecf2fff3ed94f7ae12c89
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-09-22 13:58:32 +0200

    Implement minimal functionality

--- src/db/accounts.ts
+++ src/db/accounts.ts
@@ -4,7 +4,26 @@ import Account from '../models/Account'
 
 const saveOne = async (account: Account): Promise<Account> => {
   return new Promise<Account>(async(resolve, reject) => {
-    return reject(new Error('Not Implemented'))
+    try {
+      const result = await query({
+        sql: `insert into accounts
+        (email, passwordhash, isadmin) values
+        (?, ?, ?)`,
+        values: [account.email, account.passwordHash, account.isAdmin],
+      })
+
+      const accountid = result.insertId
+
+      return resolve(new Account(
+        String(accountid),
+        account.email,
+        account.passwordHash,
+        account.isAdmin,
+      ))
+    }
+    catch(err) {
+      return reject(err)
+    }
   })
 }
 
@@ -28,7 +47,28 @@ const deleteOneById = async (id: string): Promise<boolean> => {
 
 const getOneByEmail = async (email: string): Promise<Account|null> => {
   return new Promise<Account|null>(async(resolve, reject) => {
-    return reject(new Error('Not Implemented'))
+    try {
+      const results = await query({
+        sql: `select id, email, passwordhash, isadmin
+        from accounts
+        where email = ?`,
+        values: [email],
+      })
+
+      if (results.length === 0)
+        return resolve(null)
+
+      const accountResult = results[0]
+      return resolve(new Account(
+        String(accountResult.id),
+        accountResult.email,
+        accountResult.passwordhash,
+        Boolean(accountResult.isadmin),
+      ))
+    }
+    catch(err) {
+      return reject(err)
+    }
   })
 }
 
