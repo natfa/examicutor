0c25353c63f900d6f5dd6c5bb1db0c3c275344be
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-11-01 11:57:03 +0100

    separate the express app

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,9 @@
-import express, { Request, Response, NextFunction } from 'express'
-import path from 'path';
-import session from 'express-session'
-import FileStore from 'session-file-store'
+import http from 'http';
+import app from './app';
+import config from './config/default';
 
-import questionController from './controllers/question'
-import subjectController from './controllers/subject'
-import themeController from './controllers/theme'
-import authController from './controllers/auth'
+const server = http.createServer(app);
 
-// load config
-import config from './config/default'
-
-// global config
-const app = express()
-const secret = config.sessionSecret
-const SessionStore = FileStore(session)
-
-// session config
-const sessionConfig = {
-  cookie: {
-    maxAge: 3600000,
-    httpOnly: true,
-    // TODO: turn to true when HTTPS is enabled
-    secure: false,
-  },
-  secret: secret,
-  resave: false,
-  saveUninitialized: false,
-  rolling: true,
-  unset: 'destroy',
-  store: new SessionStore({
-    retries: 1,
-    reapInterval: 43200,
-  })
-}
-
-// express config
-app.use(express.json())
-app.use(session(sessionConfig))
-app.use(reqLogger)
-
-// apply controllers
-app.use('/api/question/', questionController)
-app.use('/api/subject/', subjectController)
-app.use('/api/theme/', themeController)
-app.use('/api/auth/', authController)
-
-// serve javascript bundles
-app.use('/', express.static(path.resolve(config.clientPath)));
-
-// serve react apps with routers
-app.get('/teacher/*', (req, res) => {
-  return res.sendFile(path.resolve(config.clientPath, 'teacher/index.html'));
+server.listen(config.port, () => {
+  console.log(`Server listening on port ${config.port}`);
 });
-
-app.get('/', (req, res) => {
-  res.redirect('/landing');
-});
-
-// simple request logger
-function reqLogger(req: Request, res: Response, next: NextFunction) {
-  const log = `[${req.method}] ${req.originalUrl}`
-  console.log(log)
-  next()
-}
-
-// Start express server
-app.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}`)
-})
