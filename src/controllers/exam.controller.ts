42b43d3098c87e432374c02adbc2333ce81aa513
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-12-03 13:06:46 +0100

    implement an exam controller

---
+++ src/controllers/exam.controller.ts
@@ -0,0 +1,68 @@
+import { StrippedAnswer } from '../models/StrippedAnswer';
+import { StrippedQuestion } from '../models/StrippedQuestion';
+
+import { Exam } from '../models/Exam';
+import { StrippedExam } from '../models/StrippedExam';
+
+import examdb from '../db/exams';
+
+async function getExamById(id: string): Promise<Exam|null> {
+  const exam = await examdb.getOneById(id);
+
+  if (exam === null) return null;
+
+  // get rid of the password hash
+  delete exam.creator.passwordHash;
+
+  return exam;
+}
+
+/**
+ * Strips an exam from all fields that aren't supposed to be sent when an exam is being solved.
+ * That includes:
+ *  - removing the correct property from each answer
+ *  - removing points, theme and subjet properties from each question
+ *  - removing the endDate and creator properties from the exam
+ *
+ *  @param {Exam} The exam to be stripped
+ *
+ *  @returns {StrippedExam} The same exam with less properties
+ */
+function stripExam(exam: Exam|null): StrippedExam|null {
+  if (exam === null) return null;
+
+  const questions: StrippedQuestion[] = exam.questions.map((question) => {
+    const answers: StrippedAnswer[] = question.answers.map((answer) => {
+      if (answer.id === undefined) throw new Error('Answer.id is undefined');
+      const strippedAnswer: StrippedAnswer = {
+        id: answer.id,
+        text: answer.text,
+      };
+
+      return strippedAnswer;
+    });
+
+    if (question.id === undefined) throw new Error('Question.id is undefined');
+
+    return {
+      id: question.id,
+      text: question.text,
+      answers,
+    };
+  });
+
+  if (exam.id === undefined) throw new Error('Exam.id is undefined');
+
+  return {
+    id: exam.id,
+    name: exam.name,
+    startDate: exam.startDate,
+    timeToSolve: exam.timeToSolve,
+    questions,
+  };
+}
+
+export default {
+  getExamById,
+  stripExam,
+};
