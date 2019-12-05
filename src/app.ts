e66a7831533fc9179b3d1e89a1d3be0de1d03e42
Nikola Anastasov Velichkov <federlizer@gmail.com>
2019-12-05 17:21:18 +0100

    improve documentation

--- src/app.ts
+++ src/app.ts
@@ -61,7 +61,7 @@ app.use('/api/course/', courseRouter);
 // serve javascript bundles
 app.use('/', express.static(path.resolve(config.clientPath)));
 
-// serve react apps with routers
+// serving react apps that have react-router enabled
 app.get('/teacher/*', (_, res) => res.sendFile(path.resolve(config.clientPath, 'teacher/index.html')));
 app.get('/student/*', (_, res) => res.sendFile(path.resolve(config.clientPath, 'student/index.html')));
 
