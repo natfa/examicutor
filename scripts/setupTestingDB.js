aeb35dcac7227a5c4e64d5613e9db0689a945954
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-01-30 13:21:11 +0100

    implement a testDB setup script

---
+++ scripts/setupTestingDB.js
@@ -0,0 +1,64 @@
+/**
+ * Runs a sequence of commands that sets up the database in a way for the application to be
+ * testable by the integration tests
+ */
+
+const bcrypt = require('bcryptjs');
+const path = require('path');
+const mysql = require('mysql');
+const fs = require('fs').promises;
+const promisify = require('util').promisify;
+
+const cfgInit = require('../src/config/default.js');
+const config = cfgInit();
+
+console.log(config);
+
+const scriptPath = path.resolve(__dirname, '../sql-scripts/create-tables.sql');
+const connection = mysql.createConnection({
+  host:               config.db.host,
+  user:               config.db.user,
+  password:           config.db.password,
+  database:           config.db.database,
+  multipleStatements: true,
+});
+
+
+const query = promisify(connection.query).bind(connection);
+connection.connect();
+
+
+fs.readFile(scriptPath, 'utf8')
+
+.then((createTablesScript) => {
+  console.log('Sending queries...');
+  return query(createTablesScript)
+})
+
+.then(() => {
+  console.log('Inserting test account...');
+  return insertDummyAccount(query)
+})
+
+.then(() => {
+  connection.end();
+  console.log('Done');
+})
+
+.catch((err) => {
+  connection.end();
+  console.error(err);
+});
+
+async function insertDummyAccount(query) {
+  const saltRounds = 10;
+  const email = 'test@mail.com';
+  const hash = await bcrypt.hash('123456', saltRounds);
+
+  await query(
+    `insert into accounts
+    (email, passwordhash, roles) values
+    (?, ?, 'teacher')`,
+    [email, hash],
+  );
+}
