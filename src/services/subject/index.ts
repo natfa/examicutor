dd17469a4644b8087e4265a9e28952dc8d5dbbd5
Nikola Anastasov Velichkov <nikola@autopi.io>
2020-11-16 11:00:08 +0100

    Update subject service to use Sequelize models

--- src/services/subject/index.ts
+++ src/services/subject/index.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
-import subjectdb from '../../db/subjects';
+import { Subject } from '../../models/Subject';
 
 async function getSubjects(_: Request, res: Response, next: NextFunction): Promise<void> {
   try {
-    const subjects = await subjectdb.getAll();
+    const subjects = await Subject.findAll();
     res.status(200).send(subjects);
   } catch (err) {
     next(err);
