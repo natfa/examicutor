7e90038439908f3190f7276460db0ca4ac69bd34
Nikola Anastasov Velichkov <nikola@autopi.io>
2020-12-18 11:37:32 +0100

    Refactor program start

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,89 @@
+import dayjs from 'dayjs';
+import dayjsDuration from 'dayjs/plugin/duration';
 import process from 'process';
 import http from 'http';
-import { pool } from './db/index';
 import app from './app';
+import bcrypt from 'bcryptjs';
 
 import config from './config/default';
 
-const server = http.createServer(app);
+import {
+  sequelize,
+  User,
+  Role,
+  Module,
+  Specialty,
+} from './models';
 
-server.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}`);
-});
+dayjs.extend(dayjsDuration);
 
-// graceful shutdown
-function gracefulShutdown() {
-  console.log('Stopping express app.')
-  server.close((err) => {
-    if (err) {
-      console.error(err);
+sequelize.sync({ force: true })
+  .then(() => {
+    console.log('Creating roles');
+
+    return Role.bulkCreate([
+      { name: 'admin' },
+      { name: 'teacher' },
+      { name: 'student' },
+    ]);
+  }).then(() => {
+    console.log('Generating hash');
+    return bcrypt.hash('@lokinSkywalker&5101', 10);
+  }).then((hash) => {
+    console.log('Creating user');
+
+    return User.create({
+      email: 'federlizer@gmail.com',
+      passwordHash: hash,
+      roleId: 2,
+    });
+  })
+  .then((user) => {
+    console.log(user.toJSON());
+
+    return Module.bulkCreate([
+      { name: 'Math', themes: [
+        { name: 'Algebra' },
+        { name: 'Geometry' },
+      ]},
+      { name: 'Programming', themes: [
+        { name: 'Languages' },
+        { name: 'Syntax and semantics' },
+      ]},
+    ], {
+      include: [Module.associations.themes],
+    });
+  })
+  .then(() => {
+    return Specialty.bulkCreate([
+      { name: 'Computer Science' },
+      { name: 'Business' },
+    ]);
+  })
+  .then(() => {
+    const server = http.createServer(app);
+    server.listen(config.port, () => {
+      console.log(`Server listening on port ${config.port}`);
+    });
+
+    // graceful shutdown
+    function gracefulShutdown() {
+      console.log('Stopping express app.')
+      server.close((err) => {
+        if (err) {
+          console.error(err);
+        }
+
+        console.log('Stopping DB');
+        sequelize.close().then(() => process.exit(0));
+      });
     }
 
-    console.log('Stopping DB');
-    pool.end((err) => {
-      if (err) {
-        console.error(err);
-      }
-      process.exit(0)
+    process.on('SIGTERM', gracefulShutdown);
+    process.on('SIGINT', gracefulShutdown)
+
+    process.on('exit', () => {
+      console.log('Stopping process.');
     });
-  });
-}
-
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGINT', gracefulShutdown)
-
-process.on('exit', () => {
-  console.log('Stopping process.');
-});
+  })
+  .catch((err) => console.error(err));
